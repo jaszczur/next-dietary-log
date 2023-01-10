e9a6ac16c2f6239ab7aa1cbdd1264ea671963bf4
Piotr Jaszczyk <piotr.jaszczyk@fortum.com>
2023-01-10 17:27:47 +0100

    Migrate to Turbopack (alpha)

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig = { 
+  reactStrictMode: true, // Recommended for the `pages` directory, default in `app`.
   experimental: {
     appDir: true,
   },
